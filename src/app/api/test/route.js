df6773b4930034348f72bc1815e748f1759fa81d
shahabmusharraf <smusharraf@master-works.sa>
2025-03-12 03:31:01 +0530

    fixed ts issue

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,4 +1,5 @@
 export async function GET(req) {
+    console.log(req)
     return new Response(JSON.stringify({ message: "API is working!" }), {
       headers: { "Content-Type": "application/json" },
     });
